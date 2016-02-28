f698aba558c926d726992fa4883628fbd8fa36f8
andreij6 <andreij6@gmail.com>
2016-02-28 03:11:10 -0500

    Initial commit

---
+++ mobile/src/main/java/com/creativejones/andre/fitnext/TileEventReceiver.java
@@ -0,0 +1,21 @@
+package com.creativejones.andre.fitnext;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.microsoft.band.tiles.TileEvent;
+
+public class TileEventReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, MainActivity.class);
+        i.setAction(intent.getAction());
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        i.putExtra(context.getString(R.string.intent_key), context.getString(R.string.intent_value));
+        i.putExtra(TileEvent.TILE_EVENT_DATA, intent.getParcelableExtra(TileEvent.TILE_EVENT_DATA));
+        context.startActivity(i);
+    }
+}
